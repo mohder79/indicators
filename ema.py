82dcbba5b5bf6ee9625c39ab4a732ebde2278dda
mohder <mohder1379@gmail.com>
2022-07-13 20:33:56 +0200

    Update ema.py

--- ema.py
+++ ema.py
@@ -38,25 +38,26 @@ def fetch(symbol: str, timeframe: str, limit: int):
 
 
 # { function for calculate EMA metod 1
-def EMA(data: str, column: str, length: int):
-    return data[column].ewm(span=length, min_periods=length, adjust=False).mean()
+def EMA(data: str, src: str, length: int):
+    return data[src].ewm(span=length, min_periods=length, adjust=False).mean()
 # }
 
 
 # { function for calculate EMA metod 2
-def EMA2(data, src, Length: int):
+def EMA2(data: str, src: str, Length: int, smoothing=2):
     size = np.size(data[src]) - Length + 1
     EMA = np.zeros(np.size(data[src]))
     EMA[0] = np.mean(data[src][:Length])
+    print(EMA[0])
     for i in range(1, size):
-        EMA[i] = EMA[i-1] + (2/(Length + 1)) * \
-            ((data[src][i+Length-1]) - EMA[i-1])
+        EMA[i] = (((2/(Length + 1)) * data[src][i+Length-1]) +
+                  ((1-(2/(Length + 1))) * EMA[i-1]))
     return EMA
 # }
 
 
 # { set the symbol for data function
-BTC = fetch('BTC/USDT:USDT', '1h', 200)
+BTC = fetch('BTC/USDT:USDT', '1d', 200)
 # }
 
 BTC['ema1'] = EMA(BTC, 'close', 5)  # use function for calculate ema metod 1
@@ -67,8 +68,8 @@ BTC['ema2'] = BTC['ema2'].shift(4)
 
 BTC['emata'] = ta.ema(BTC.close, 5)  # use ta lib for calculate ema
 
-
 print(BTC)
+
 # { plot the data
 plt.plot(BTC.close, label='closes price')
 plt.plot(BTC.ema1, label='EMA')
